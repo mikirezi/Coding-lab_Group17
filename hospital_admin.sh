5f523154509721247906792ed6e262903b539a86
Joshua Gasasira <j.gasasira@alustudent.com>
2026-06-03 15:58:32 +0300

    Member 2: implemented secure_data permissions

--- hospital_admin.sh
+++ hospital_admin.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Member 3 - Orchestrator (System Controller)
 
 echo "Starting Hospital Admin System..."
@@ -10,13 +9,15 @@ initialize_system() {
 }
 
 secure_data() {
-    echo "Securing data..."
+    echo "Securing active_logs..."
+    chmod 700 active_logs
+    ls -l
     # TODO: Member 2 will implement security logic
-}
+} 
 
 # Execution flow
 initialize_system
 secure_data
 
 echo "System Environment Secured"
-echo "Date: $(date)"
+echo "Date: $(date)
