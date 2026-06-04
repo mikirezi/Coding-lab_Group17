c9ec52a46f03a04e376510ed65e0f923fa22d16d
mikirezi <mikirezi@example.com>
2026-06-04 04:26:14 -0700

    Final integration of security and archive fixes

--- hospital_admin.sh
+++ hospital_admin.sh
@@ -1,47 +1,37 @@
 #!/bin/bash
 
-# Member 3 - Orchestrator (System Controller)
-
 echo "Starting Hospital Admin System..."
 
 initialize_system() {
-      echo "Initializing KNH system environment..."
-    # Written by Member 1 (Architect)
-    
+    echo "Initializing KNH system environment..."
+
     if [ ! -d "active_logs" ]; then
-        echo "Creating active_logs directory..."
         mkdir active_logs
-    else
-        echo "active_logs already exists."
     fi
-    
+
     if [ ! -d "archived_logs" ]; then
-        echo "Creating archived_logs directory..."
         mkdir archived_logs
-    else
-        echo "archived_logs already exists."
     fi
-    
+
     if [ ! -d "reports" ]; then
-        echo "Creating reports directory..."
         mkdir reports
-    else
-        echo "reports already exists."
     fi
-    
-    echo "System initialization completed successfully."
-   
+
+    echo "System initialization completed successfully"
 }
 
 secure_data() {
     echo "Securing data..."
-    # TODO: Member 2 will implement security logic
+
+    chmod 700 active_logs
+    chmod 700 archived_logs
+    chmod 700 reports
+
+    echo "System Environment Secured"
 }
 
 # Execution flow
 initialize_system
 secure_data
 
-echo "System Environment Secured"
 echo "Date: $(date)"
-
